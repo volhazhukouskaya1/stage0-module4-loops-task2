2db3ee6e098241d052eb4cd1be3ada1a54ac7ad5
volhazhukouskaya1 <volha_zhukouskaya@outlook.com>
2024-05-08 14:33:13 +0300

    task 4-2-2(5)

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,20 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
-
-        if (power < 0) {
+       if (power < 0) {
             System.out.println("too much power");
         }
-        else if (power == 0) {
-            System.out.println(1);}
-        else {
-          int counter=1;
+       else {
+          int counter =0;
+          int result = 1;
             while (counter <= power) {
 
-               System.out.println(counter);
-               counter *=2;
+               System.out.println(result);
+               result *=2;
+               counter++;
             }
         }
     }
 }
+
+
